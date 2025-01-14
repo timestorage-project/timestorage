ce9437f5bed6a1ddeba746acf857fea16b251282
JustReitek <rodolfopedace01@gmail.com>
2025-01-14 11:42:51 +0100

    Update default canister ID

--- src/timestorage_frontend/src/services/canisterService.ts
+++ src/timestorage_frontend/src/services/canisterService.ts
@@ -18,7 +18,7 @@ const initializeAgent = async () => {
   if (!timestorageActor) {
     timestorageActor = Actor.createActor<TimestorageBackend>(idlFactory, {
       agent,
-      canisterId: process.env.CANISTER_ID_TIMESTORAGE_BACKEND || 'bd3sg-teaaa-aaaaa-qaaba-cai'
+      canisterId: process.env.CANISTER_ID_TIMESTORAGE_BACKEND || 'gc5gl-leaaa-aaaaa-qaara-cai'
     })
   }
 
